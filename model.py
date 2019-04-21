1b7d6844737c6a7d47ec294a7485bf97c5f55093
Yifan Qiao <qiao-yf15@mails.tsinghua.edu.cn>
2019-04-21 14:10:19 +0800

    clean model.py

--- model.py
+++ model.py
@@ -51,14 +51,3 @@ class GGNN(nn.Module):
         h = self.g.ndata.pop('h')
         h = self.out(h)
         return h
-
-
-def evaluate(model, features, labels, mask):
-    model.eval()
-    with torch.no_grad():
-        logits = model(features)
-        logits = logits[mask]
-        lables = lables[mask]
-        _, indices = torch.max(logits, dim=1)
-        correct = torch.sum(indices == labels)
-        return correct.item() * 1.0 / len(labels)
