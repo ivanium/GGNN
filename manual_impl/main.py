426ffca1a353fa4d252f59fcbde87756c0f77cf1
Yifan Qiao <qiao-yf15@mails.tsinghua.edu.cn>
2019-04-22 11:31:30 +0800

    manual version 0. can train but loss inf

--- manual_impl/main.py
+++ manual_impl/main.py
@@ -6,15 +6,12 @@ import numpy.random as nprand
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
 
-from dgl import DGLGraph
 from dgl.data import register_data_args
-# from dgl.data import load_data
 
 from util import load_data
-
 from model import GGNN
+
 
 def evaluate(model, features, labels, mask):
     model.eval()
@@ -37,32 +34,36 @@ def main(args):
     val_mask = torch.ByteTensor(data.val_mask)
     test_mask = torch.ByteTensor(data.test_mask)
 
+    g = data.graph
     num_classes = data.num_labels
-    num_edges = data.graph.number_of_edges()
+    num_edges = g.num_edges
     feature_dim = features.shape[1]
     num_edge_type = 2
 
-    edge_types = Variable((torch.rand([num_edges]).squeeze() > 0.5).long()) # random assign edge type for all edges
 
     if args.gpu < 0:
         cuda = False
     else:
         cuda = True
         torch.cuda.set_device(args.gpu)
+        g.edge_types = g.edge_types.cuda()
+        g.edge_list = g.edge_list.cuda()
+        g.src_list = g.src_list.cuda()
+        g.dst_list = g.dst_list.cuda()
+        g.edge_types = g.edge_types.cuda()
+
         features = features.cuda()
-        edge_types = edge_types.cuda()
         labels = labels.cuda()
         train_mask = train_mask.cuda()
         val_mask = val_mask.cuda()
         test_mask = test_mask.cuda()
 
-    g = DGLGraph(data.graph)
-    g.edata['e'] = edge_types
-
     model = GGNN(g, num_classes, num_edge_type, feature_dim, feature_dim)
 
     if cuda:
         model = model.cuda()
+        for i in range(num_edge_type):
+                model.edge_matrix[i] = model.edge_matrix[i].cuda()
 
     loss_criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(),
@@ -91,23 +92,24 @@ def main(args):
                                              acc, num_edges / np.mean(dur) / 1000))
 
     print()
-    acc = evaluate(model, features, lables, test_mask)
+    acc = evaluate(model, features, labels, test_mask)
     print("Test Accuracy {:.4f}".format(acc))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GGNN')
-    register_data_args(parser)
+    parser.add_argument("--dataset", type=str, default="ppi",
+                        help="graph dataset dir")
     parser.add_argument("--gpu", type=int, default=-1,
-            help="gpu")
+                        help="gpu")
     parser.add_argument("--lr", type=float, default=1e-2,
-            help="learning rate")
-    parser.add_argument("--n-epochs", type=int, default=200,
-            help="number of training epochs")
+                        help="learning rate")
+    parser.add_argument("--n-epochs", type=int, default=20,
+                        help="number of training epochs")
     parser.add_argument("--hidden-dim", type=int, default=50,
-            help="gru hidden state dimension")
+                        help="gru hidden state dimension")
     parser.add_argument("--weight-decay", type=float, default=5e-4,
-            help="Weight for L2 loss")
+                        help="Weight for L2 loss")
     args = parser.parse_args()
     print(args)
 
---
+++ manual_impl/model.py
@@ -0,0 +1,59 @@
+import math
+import torch
+import torch.nn as nn
+from torch_scatter import scatter_add
+from torch.autograd import Variable
+
+
+class GGNN(nn.Module):
+    def __init__(self,
+                 g,
+                 num_classes,
+                 num_edge_type,
+                 msg_dim=10,
+                 hidden_dim=10):
+        super(GGNN, self).__init__()
+        self.g = g
+        self.msg_dim = msg_dim
+        self.hidden_dim = hidden_dim
+        self.num_classes = num_classes
+        self.edge_matrix = []
+        for i in range(num_edge_type):
+            self.edge_matrix.append(nn.Linear(hidden_dim, msg_dim))
+        self.gru = nn.GRU(input_size=msg_dim, hidden_size=hidden_dim)
+        # Output Model
+        self.out = nn.Linear(hidden_dim, num_classes)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        pass
+
+    def forward(self, features):
+        typed_nodes = []
+        typed_nodes.append(self.g.src_list[self.g.edge_types])
+        typed_nodes.append(self.g.src_list[self.g.edge_types == 0])
+        # filter features according to edge type
+        typed_features = []
+        for i in range(2):
+            typed_features.append(features[typed_nodes[i]])
+
+        # num(edge type) times dense matrix multiply
+        typed_msgs = []
+        for i in range(2):
+            typed_msgs.append(self.edge_matrix[i](typed_features[i]))
+
+        # message passing && reduce message
+        reduced_msgs = features.new_empty(size=features.shape).t()
+        scatter_add(typed_msgs[0].t(), typed_nodes[0].unsqueeze(1).expand(typed_msgs[0].shape).t(), out=reduced_msgs)
+        reduced_msgs = reduced_msgs + \
+            scatter_add(typed_msgs[1].t(), typed_nodes[1].unsqueeze(1).expand(typed_msgs[1].shape).t(), dim_size=self.g.num_nodes)
+
+        # batched messages and features feed into gru unit
+        _, h = self.gru(
+            reduced_msgs.t().unsqueeze(0),
+            features.unsqueeze(0)
+        )
+
+        # output layer
+        h = self.out(h[0])
+        return h
